8a178db34db44d824befce5e532fd952be81f51e
housejames <housejames14@gmail.com>
2024-03-30 20:59:14 -0700

    fixing routes and connections

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+require("dotenv").config();
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const allRoutes = require('./routes');
+const sequelize = require('./config/connection');
 
+// Sets up the Express App
+// =============================================================
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(routes);
+app.use('/',allRoutes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+    });
 });
